1b8c628da1aa4e3421ac20b90bef517f6a534804
ricardo <ricardoluiz_p@hotmail.com>
2019-10-21 20:08:56 -0300

    r operadores

---
+++ cap02/02-Operadores.R
@@ -0,0 +1,53 @@
+#diretorio
+setwd('D:/#dev/scripts-dataset-r/cap02')
+getwd()
+
+##Opeçaões
+
+#soma
+7 + 7
+
+#subtração
+7 - 4
+
+#mutilplicação
+5 * 5
+
+#divisão
+6 / 6
+
+#potência
+3^2
+3**2
+
+#módulo
+16 %% 3
+
+##Operadores Relacionais
+
+#atribuindo variaveis
+x = 7
+y = 5
+
+#operadores
+x > 8 
+x < 8 
+x <= 8
+x >= 8
+x == 8
+x != 8
+
+##operadores lógicos
+
+#And
+(X==8) & (x==6)
+(X==7) & (x==5)
+(X==8) & (x==5)
+
+#Or
+(x==8) | (x>5)
+(x==8) | (x>=5)
+
+#not
+x>8
+print(!x>8)
